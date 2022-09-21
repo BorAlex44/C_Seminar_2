56b11ddd585c8d7b425347e6ea7aba3840fba95c
BorAlex <alexander44@bl.ru>
2022-09-21 21:31:22 +0300

    add example2_10

--- Example2_10/Program.cs
+++ Example2_10/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿using static System.Console;
+Clear();
+WriteLine("Введите любое трехзначное число: ");
+int number = int.Parse(ReadLine()!);
+//int temp = number / 10;
+//int result = temp % 10;
+int result = (number / 10) - (number / 100) * 10;
+WriteLine($"Вторая цифра числа {number} это {result}");

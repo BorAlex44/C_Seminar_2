0e4945aba5ad3ffd3592f2462895a3cb357bd94f
BorAlex <alexander44@bl.ru>
2022-09-22 19:18:42 +0300

    add Example2_13

---
+++ Exanple2_13/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+Clear();
+WriteLine("Введите любое число: ");
+int number = int.Parse(ReadLine()!);
+int temp = 0;
+if (number / 100 == 0)
+{
+    WriteLine("У данного числа нет третьей цифры");
+    return;
+}
+if (number / 1000 == 0)
+{
+    temp = number % 10;
+}
+while (number / 1000 != 0)
+{
+    number = number / 10;
+    temp = number % 10;
+}
+WriteLine($"Третья цифра введенного числа - {temp}");
